983ddfb0747e9226c1aec0534ae2a4872fa39291
jaycossey <jaycossey@live.com>
2023-10-24 18:32:37 +0100

    added tailwind css setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './assets/scripts/scripts.js',
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

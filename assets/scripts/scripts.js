728dfc22c655693b776c95d6c5e4be40f75352e1
jaycossey <jaycossey@live.com>
2023-10-26 14:02:14 +0100

    added basic styles and setup required planning in JS file, edited readme, removed tailwind

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,28 @@
+// GLOBALS
+const GENERATE_BUTTON = document.getElementById('generate');
+const TEXT_DISPLAY = document.getElementById('instructions');
+
+// validation regex
+const REGEX = "change to regex";
+
+// desired length of password
+let desiredLength = 10;
+
+// password character array
+let characterArray = [desiredLength + 1];
+
+// function declarations ----------------------------------
+// generate dependant on length
+function generate(length) {
+
+}
+
+// validate the password strength
+function validate() {
+
+}
+
+// display the password on screen after generation
+function displayPassword() {
+
+}
